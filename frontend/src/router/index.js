53bcd94c9d851b6c2639f149cef844eaf105f130
Mattia Cardile <mattiaca003@gmail.com>
2022-05-28 23:21:36 +0200

    Fixed formatting

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,7 +8,8 @@ import store from '../store';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+    routes:
+    [
         {
             path: '/',
             redirect: '/dashboard'
@@ -16,12 +17,14 @@ const router = createRouter({
         {
             path: '/dashboard',
             name: 'Dashboard',
-            meta: {
+            meta:
+            {
                 protected: true
             },
             component: DashboardView,
             redirect: { name: 'Tasks' },
-            children: [
+            children:
+            [
                 {
                     path: 'tasks',
                     name: 'Tasks',
